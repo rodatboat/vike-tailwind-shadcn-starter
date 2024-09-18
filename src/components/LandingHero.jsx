4d86942cbcd0ce16bd496361de35099a22a65b15
rodatboat <ronaldo3055@gmail.com>
2024-09-18 01:48:56 -0500

    updated texts and imports

--- src/components/LandingHero.jsx
+++ src/components/LandingHero.jsx
@@ -21,7 +21,7 @@ export default function LandingHero({ }) {
     return (
         <section className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 lg:items-start px-8 py-8 lg:py-20">
             <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
-                <img alt="ABAMock Logo" fetchpriority="high"
+                <img alt="Vike + Spring Logo" fetchpriority="high"
                     decoding="async" data-nimg="1" className="size-28 max-w-xl mx-auto md:mr-auto md:ml-0 -mb-16" style={{ color: "transparent" }}
                     src={logoUrl} />
                 <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4 flex flex-col gap-3 items-center lg:items-start">
@@ -51,7 +51,7 @@ export default function LandingHero({ }) {
                 </div>
             </div>
             <div className="relative max-md:-m-4 lg:w-full">
-                <img alt="ABAMock Logo" fetchpriority="high"
+                <img alt="Vike + Spring Logo" fetchpriority="high"
                     width="1080" height="1080" decoding="async" data-nimg="1" className="w-full max-w-xl ml-auto" style={{ color: "transparent" }}
                     src={landingUrl} />
             </div>
